e805c0205f3f89a4d9e0a4043be0a51efbb131bd
Chris Wiegman <chris@chriswiegman.com>
2014-11-14 08:54:14 -0600

    Add init hook and function

--- inc/class-hide-dashboard.php
+++ inc/class-hide-dashboard.php
@@ -262,6 +262,177 @@ class Hide_Dashboard {
 	}
 
 	/**
+	 * Returns the root of the WordPress install
+	 *
+	 * @since 0.0.1
+	 *
+	 * @return string the root folder
+	 */
+	public static function get_home_root() {
+
+		//homeroot from wp_rewrite
+		$home_root = parse_url( site_url() );
+
+		if ( isset( $home_root['path'] ) ) {
+
+			$home_root = trailingslashit( $home_root['path'] );
+
+		} else {
+
+			$home_root = '/';
+
+		}
+
+		return $home_root;
+
+	}
+
+	/**
+	 * Execute hide backend functionality
+	 *
+	 * @since 0.0.1
+	 *
+	 * @return void
+	 */
+	public function init() {
+
+		if ( get_site_option( 'users_can_register' ) == 1 && isset( $_SERVER['REQUEST_URI'] ) && $_SERVER['REQUEST_URI'] == $this->get_home_root() . get_site_option( 'hd_register' ) ) {
+
+			wp_redirect( wp_login_url() . '?action=register' );
+
+			exit;
+
+		}
+
+		//redirect wp-admin and wp-register.php to 404 when not logged in
+		if (
+			(
+				(
+					get_site_option( 'users_can_register' ) == false &&
+					(
+						isset( $_SERVER['REQUEST_URI'] ) && strpos( $_SERVER['REQUEST_URI'], 'wp-register.php' ) ||
+						isset( $_SERVER['REQUEST_URI'] ) && strpos( $_SERVER['REQUEST_URI'], 'wp-signup.php' )
+					)
+				) ||
+				(
+					isset( $_SERVER['REQUEST_URI'] ) && strpos( $_SERVER['REQUEST_URI'], 'wp-login.php' ) && is_user_logged_in() !== true
+				) ||
+				( is_admin() && is_user_logged_in() !== true ) ||
+				(
+					$this->settings['register'] != 'wp-register.php' &&
+					strpos( $_SERVER['REQUEST_URI'], 'wp-register.php' ) !== false ||
+					strpos( $_SERVER['REQUEST_URI'], 'wp-signup.php' ) !== false ||
+					(
+						isset( $_REQUEST['redirect_to'] ) &&
+						strpos( $_REQUEST['redirect_to'], 'wp-admin/customize.php' ) !== false
+
+					)
+				)
+			) &&
+			strpos( $_SERVER['REQUEST_URI'], 'admin-ajax.php' ) === false
+			&& $this->auth_cookie_expired === false
+		) {
+
+			global $hd_is_old_admin;
+
+			$hd_is_old_admin = true;
+
+			if ( get_site_option( 'hd_theme_compat' ) == true ) { //theme compatibility (process theme and redirect to a 404)
+
+				wp_redirect( $this->get_home_root() . sanitize_title( get_site_option( 'hd_theme_compat_slug' ) ), 302 );
+				exit;
+
+			} else { //just set the current page as a 404
+
+				add_action( 'wp_loaded', array( $this, 'set_404' ) );
+
+			}
+
+		}
+
+		$url_info                  = parse_url( $_SERVER['REQUEST_URI'] );
+		$login_path                = site_url( get_site_option( 'hd_slug' ), 'relative' );
+		$login_path_trailing_slash = site_url( get_site_option( 'hd_slug' ) . '/', 'relative' );
+
+		if ( $url_info['path'] === $login_path || $url_info['path'] === $login_path_trailing_slash ) {
+
+			if ( ! is_user_logged_in() ) {
+				//Add the login form
+
+				if ( strlen( trim( get_site_option( 'hd_post_logout_slug' ) ) ) > 0 && isset( $_GET['action'] ) && sanitize_text_field( $_GET['action'] ) == trim( get_site_option( 'hd_post_logout_slug' ) ) ) {
+
+					//add hook here for custom users... Begin deprication of itsec hook
+					do_action( 'itsec_custom_login_slug' );
+					do_action( 'hd_custom_login_slug' );
+
+				}
+
+				//suppress error messages due to timing
+				error_reporting( 0 );
+				@ini_set( 'display_errors', 0 );
+
+				status_header( 200 );
+
+				//don't allow domain mapping to redirect
+				if ( defined( 'DOMAIN_MAPPING' ) && DOMAIN_MAPPING == 1 ) {
+					remove_action( 'login_head', 'redirect_login_to_orig' );
+				}
+
+				if ( ! function_exists( 'login_header' ) ) {
+
+					include( ABSPATH . 'wp-login.php' );
+					exit;
+
+				}
+
+			} elseif ( ! isset( $_GET['action'] ) || ( sanitize_text_field( $_GET['action'] ) != 'logout' && sanitize_text_field( $_GET['action'] ) != 'postpass' && ( strlen( trim( get_site_option( 'hd_post_logout_slug' ) ) ) > 0 && sanitize_text_field( $_GET['action'] ) != trim( get_site_option( 'hd_post_logout_slug' ) ) ) ) ) {
+				//Just redirect them to the dashboard (for logged in users)
+
+				if ( $this->auth_cookie_expired === false ) {
+
+					wp_safe_redirect( get_admin_url() );
+					exit();
+
+				}
+
+			} elseif ( isset( $_GET['action'] ) && ( sanitize_text_field( $_GET['action'] ) == 'postpass' || ( strlen( trim( get_site_option( 'hd_post_logout_slug' ) ) ) > 0 && sanitize_text_field( $_GET['action'] ) == trim( get_site_option( 'hd_post_logout_slug' ) ) ) ) ) {
+				//handle private posts for
+
+				if ( strlen( trim( get_site_option( 'hd_post_logout_slug' ) ) ) > 0 && sanitize_text_field( $_GET['action'] ) == trim( get_site_option( 'hd_post_logout_slug' ) ) ) {
+
+					//add hook here for custom users: Begin deprication of itsec slug
+					do_action( 'itsec_custom_login_slug' );
+					do_action( 'hd_custom_login_slug' );
+
+				}
+
+				//suppress error messages due to timing
+				error_reporting( 0 );
+				@ini_set( 'display_errors', 0 );
+
+				status_header( 200 ); //its a good login page. make sure we say so
+
+				//include the login page where we need it
+				if ( ! function_exists( 'login_header' ) ) {
+					include( ABSPATH . '/wp-login.php' );
+					exit;
+				}
+
+				//Take them back to the page if we need to
+				if ( isset( $_SERVER['HTTP_REFERRER'] ) ) {
+
+					wp_safe_redirect( esc_url( $_SERVER['HTTP_REFERRER'] ) );
+					exit();
+
+				}
+
+			}
+
+		}
+
+	}
+
+	/**
 	 * Sanitize enabled field
 	 *
 	 * @since 0.0.1
